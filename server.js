e39ca9cac72ef0c30f5e40c3967cf59a55212990
shivamrai83 <rai.shivam@thinksys.com>
2024-08-20 18:07:03 +0530

    Basic Next.js App And Socket Server Setup

---
+++ server.js
@@ -0,0 +1,30 @@
+// server/server.js
+const http = require('http');
+const server = http.createServer((req, res) => {
+  // Handle HTTP requests if needed
+});
+
+const { Server } = require('socket.io');
+const io = new Server(server, {
+    cors: {
+      origin: "http://localhost:3000", // Update with your client app's URL
+      methods: ["GET", "POST"],
+    },
+  });
+  
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  
+  // Handle chat messages
+  socket.on('chat message', (message) => {
+    io.emit('chat message', message); // Broadcast the message to all connected clients
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+server.listen(3001, () => {
+  console.log('WebSocket server listening on port 3001');
+});
---
+++ src/app/page.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+import styles from "./page.module.css";
+
+// Create the socket instance once
+const socket = io('http://localhost:3001'); // Replace with your server URL
+
+export default function Home() {
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState('');
+
+  useEffect(() => {
+    console.log('useEffect listen');
+
+    const handleMessage = (message) => {
+      console.log('inside socket', message);
+      setMessages((prevMessages) => [...prevMessages, message]);
+    };
+
+    socket.on('chat message', handleMessage);
+
+    // Clean up the listener on component unmount
+    return () => {
+      socket.off('chat message', handleMessage);
+    };
+  }, []);
+
+  const sendMessage = () => {
+    console.log('send');
+    socket.emit('chat message', newMessage);
+    setNewMessage('');
+  };
+
+  console.log(messages);
+
+  return (
+    <main className={styles.main}>
+      <div>
+        <h1>Real-Time Chat</h1>
+        <div>
+          {messages.map((message, index) => (
+            <div key={index}>{message}</div>
+          ))}
+        </div>
+        <input
+          type="text"
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
+        />
+        <button onClick={sendMessage}>Send</button>
+      </div>
+    </main>
+  );
+}
