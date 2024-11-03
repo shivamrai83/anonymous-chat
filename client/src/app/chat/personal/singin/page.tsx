8bc4acb6cfa9eee286a7601f4c854f9ba68715d9
shivamrai83 <shivamrai83@gmail.com>
2024-11-03 21:31:30 +0530

    Chat App: Personal Chat Flow :1.1.3

--- client/src/app/chat/personal/singin/page.tsx
+++ client/src/app/chat/personal/singin/page.tsx
@@ -19,7 +19,7 @@ const SingIn = () => {
   const router = useRouter()
   console.log('personalChatSocketId',personalChatSocketId)
 
-  // refresh code
+  // refresh wala code
   useEffect(()=>{
   //   if(localStorage.getItem('userName')){
   //     socket.emit(`PERSONAL_NEW_USER`, { userName, socketID: socket.id });
