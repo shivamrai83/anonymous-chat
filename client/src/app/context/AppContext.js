a1f0714b8fd68a7ceabe49d0a96ade1fec89bd57
shivamrai83 <shivamrai83@gmail.com>
2024-11-09 23:37:50 +0530

    Chat App: Personal Changed Flow Room :1.1.4

--- client/src/app/context/AppContext.js
+++ client/src/app/context/AppContext.js
@@ -1,11 +1,9 @@
 "use client"
 import React, { createContext, useState } from 'react';
-
 const AppContext = createContext();
 
 const AppProvider = ({ children }) => {
     const [chatType, setChatType] = useState('');
-    // const [personalChatRoomId, setPersonalChatRoomId] = useState(null);
     const [roomId, setRoomId] = useState(uuidv4());
     const [socketExist, setSocketExists] = useState(false);
 
